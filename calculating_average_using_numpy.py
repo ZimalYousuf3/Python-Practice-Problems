63392e38e35f8e59fac0a373e97624d467f1b21e
Zimal Yousuf <zimalyousuf0301@gmail.com>
2026-07-18 17:56:44 +0500

    calculating_average_using_numpy.py

--- calculating_average_using_numpy.py
+++ calculating_average_using_numpy.py
@@ -1,5 +1,7 @@
 # Create a 2D array of 3 students × 3 subjects of scores; print each student's average (arr.mean(axis=1)
 
+import numpy as np
+
 scores = np.array([[78, 56, 33], [99, 87, 82], [78, 93, 86]])
 
 print ('Array: ')
